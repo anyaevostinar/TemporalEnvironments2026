dc87394128ce2b86b82a896549adac2637f90024
Lauren <laurenpcaldwell@gmail.com>
2026-06-29 19:10:46 -0500

    fixed typo

--- Data/26_6_29/run-parasites-no-change.sh
+++ Data/26_6_29/run-parasites-no-change.sh
@@ -12,7 +12,7 @@
 #SBATCH -o np%A_%a.out
 
 ## Memory requirement in megabytes. You might need to make this bigger.
-#SBATCH --mem-per-cpu=10ß00M
+#SBATCH --mem-per-cpu=1000M
 
 ## Launch an array of jobs. This determines your random seeds
 #SBATCH --array=100-129
