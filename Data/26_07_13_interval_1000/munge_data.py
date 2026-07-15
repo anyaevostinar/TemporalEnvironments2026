d75b6011c7855d705de22bdb76b200817869eafd
LaurenC267 <caldwelll@carleton.edu>
2026-07-15 16:28:01 -0500

    new munge file

--- Data/26_07_13_interval_1000/munge_data.py
+++ Data/26_07_13_interval_1000/munge_data.py
@@ -13,6 +13,11 @@ outputFileName = "munged_basic_tasks.dat"
 
 outFile = open(outputFileName, 'w')
 outFile.write(header)
+count = 0
+poison = -1
+neutral = 0
+reward = 1 
+poison_or_not = 0
 
 for t in treatment_folders:
     for r in reps:
@@ -21,12 +26,31 @@ for t in treatment_folders:
         curFile = open(fname, 'r')
         for line in curFile:
             if (line[0] != "u"):
+                count +=1
                 splitline = line.strip().split(',')
+
                 for task in range(1,10): # nine logic tasks
-                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task_names[task], splitline[task], "Host")
+                    if ((task_names[task]=="AND") or (task_names[task] == "OR") or task_names[task] == "NOT"):
+                        poison_or_not = poison
+                    elif ((task_names[task]=="NAND") or (task_names[task] == "OR_NOT") or task_names[task] == "AND_NOT"):
+                        poison_or_not = reward
+                    else:
+                        poison_or_not = neutral
+                    outstring1 = "{} {} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task_names[task], splitline[task], poison_or_not, "Host")
                     outFile.write(outstring1)
                 for task in range(10,19): # nine logic tasks
-                    outstring1 = "{} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task_names[task-9], splitline[task], "Sym")
+                    if ((task_names[task]=="AND") or (task_names[task] == "OR") or task_names[task] == "NOT"):
+                        poison_or_not = poison
+                    elif ((task_names[task]=="NAND") or (task_names[task] == "OR_NOT") or task_names[task] == "AND_NOT"):
+                        poison_or_not = reward
+                    else:
+                        poison_or_not = neutral
+                    outstring1 = "{} {} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task_names[task-9], splitline[task], poison_or_not, "Sym")
                     outFile.write(outstring1)
+            
+                if (count % 1000 == 0):
+                    reward, poison = poison, reward
+            
+            
         curFile.close()
 outFile.close()
