ce3d8dff593a2d3042877d3d5ba4b53cb96a3b96
LaurenC267 <caldwelll@carleton.edu>
2026-07-16 13:26:51 -0500

    munge

--- Data/26_07_15_noSym_diff/munge_data.py
+++ Data/26_07_15_noSym_diff/munge_data.py
@@ -1,7 +1,7 @@
 import os.path
 import gzip
 
-folder = '../../Data/26_07_13_interval_1000'
+folder = '../../Data/26_07_15_noSym_diff'
 
 treatment_folders = ["run_parasites_changing"]
 reps = range(100,130)
